16b5ad2c2e5fc4f59328b2d90b5e934dc859929a
AndrewFoxATU <G00417373@atu.ie>
2023-12-05 13:36:47 +0000

    Person

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,6 +2,8 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person myPerson = new Person("Andrew", "Galway", "087212121");
+        System.out.printIn(myPerson.toString());
     }
+
 }
---
+++ src/main/java/ie/atu/Person.java
@@ -0,0 +1,53 @@
+package ie.atu;
+
+public  class Person {
+    private String name;
+    private String address;
+    private String phoneNum;
+
+    // Default constructor
+    public Person() {
+        this.name = "";
+        this.address = "";
+        this.phoneNum = "";
+    }
+    // Constructor
+    public Person(String name, String address, String phoneNum) {
+        this.name = name;
+        this.address = address;
+        this.phoneNum = phoneNum;
+    }
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    // Setters
+    public void setName(String getName) {
+        this.name = name;
+    }
+    public void setAddress(String getAdress) {
+        this.address = address;
+    }
+    public void setPhoneNum(String getPhoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNum='" + phoneNum + '\'' +
+                '}';
+    }
+}
+
+//
